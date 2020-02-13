685a9deba9275701457c0982c0562dd5f982c4eb
Paul Hansa <5748299+biggestcookie@users.noreply.github.com>
2020-02-13 14:57:21 -0500

    Update player.py

--- game/player.py
+++ game/player.py
@@ -9,9 +9,9 @@ class Player:
 
     def __init__(self):
         if (len(self.team) < 1):
-            self.team.append(self.assign_random_pokemon())
+            self.team.append(self.get_random_pokemon())
 
-    def assign_random_pokemon(self) -> Pokemon:
+    def get_random_pokemon(self) -> Pokemon:
         # TODO: actually random pokemon
         random_pokemon = Pokemon("Eevee", 133, typechart.NORMAL)
         return random_pokemon
