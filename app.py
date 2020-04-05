5d06fdc8d22a2203ad3e56ae4e0c220afa44c70a
Paul <5748299+biggestcookie@users.noreply.github.com>
2020-04-05 22:22:37 +0000

    Remove redundant function

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ from states.menu import Menu
 
 def main():
     """Entry point for the application"""
-    init_player()
-    Menu()
-
-
-def init_player():
-    """Starts PLAYER instance and inform user of their first Pokemon"""
     PLAYER.print_team()
+    Menu()
 
 
 if __name__ == "__main__":
