67c641e082680f6f333012ae3b8597312dda82c0
PORYGON\Kevin <kevinla479@gmail.com>
2020-04-07 18:03:22 -0400

    Added Loop for Team and proper text in text.json

--- states/team.py
+++ states/team.py
@@ -1,7 +1,35 @@
+from typing import Optional
+
+from config.config import TEXT
+from states.battle import Battle
+from states.state import State, StateOptions
 
 
-class Team:
+class TeamOptions(StateOptions):
+    """Enum values for options availabe in team menu"""
+
+    TOSSPKMN = 1
+    REORDER = 2
+    EXIT = 3
+
+
+class Team(State):
     """Game state for player team management"""
+
+    option: Optional[TeamOptions] = None
 
     def __init__(self):
         super().__init__()
+        while self.option is not TeamOptions.EXIT:
+            print(TEXT["TEAM"]["ENTRY"])
+            self.option = self.check_input(TeamOptions)
+            self.choose_option()
+
+    def choose_option(self):
+        """Enters next game state based on player choice"""
+        if self.option == TeamOptions.TOSSPKMN:
+            Battle()
+        elif self.option == TeamOptions.REORDER:
+            Team()
+        elif self.option == TeamOptions.EXIT:
+            print(TEXT["TEAM"]["EXIT"])
