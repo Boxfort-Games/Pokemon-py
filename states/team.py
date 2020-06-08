d9a471fd9871ce21663fc0b2bcb8e03e06c191e8
Paul Hansa <5748299+biggestcookie@users.noreply.github.com>
2020-06-07 21:08:41 -0400

    Move toss method to player class for reusability

--- states/team.py
+++ states/team.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-from readchar import readkey
-
 from config.config import TEXT
 from game.player import PLAYER
 from states.state import State, StateOptions
@@ -30,51 +28,9 @@ class Team(State):
     def choose_option(self):
         """Performs team action based on player choice"""
         if self.option == TeamOptions.TOSSPKMN:
-            Toss()
+            PLAYER.remove_from_team()
         elif self.option == TeamOptions.REORDER:
             pass
-
-
-class Toss(State):
-    """Substate for tossing Pokemon from team"""
-
-    is_tossing: bool = True
-
-    def __init__(self):
-        super().__init__()
-        while self.is_tossing:
-            if len(PLAYER.team) > 1:
-                print(TEXT["TEAM"]["TOSS"])
-                print(
-                    *[f"{str(i+1)}. {slot.name}" for i, slot in enumerate(PLAYER.team)],
-                    sep="\n",
-                )
-                print(TEXT["TEAM"]["EXIT"], end="\n" * 2)
-
-                self.is_tossing = self.attempt_toss()
-            else:
-                print(TEXT["TEAM"]["SIZE_ERROR"], end="\n" * 2)
-                self.is_tossing = False
-
-    @staticmethod
-    def attempt_toss() -> bool:
-        """Receives user input and attempts to toss a pokemon"""
-        try:
-            choice = readkey()
-            player_toss_choice = PLAYER.team[int(choice) - 1]
-            print(
-                f"{player_toss_choice.name} {TEXT['TEAM']['RESULT']} {player_toss_choice.name}!",
-                end="\n" * 2,
-            )
-            PLAYER.team.remove(player_toss_choice)
-            return False
-        except IndexError:
-            # Index not found in PLAYER.team
-            print(TEXT["TEAM"]["ERROR"], end="\n" * 2)
-            return True
-        except ValueError:
-            # Key other than number was pressed
-            return False
 
 
 class ReorderOptions(StateOptions):
