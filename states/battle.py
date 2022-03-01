330c561e0fe5fb657ff03300265fac28b236e1f4
lyricalrain <22138224+lyricalrain@users.noreply.github.com>
2022-03-01 23:56:56 +0000

    Added run option probability calculator and run method to battle state, leader_pokemon attribute to player class

--- states/battle.py
+++ states/battle.py
@@ -1,8 +1,10 @@
 import asyncio
+import random
 from typing import Optional
 
 from api import pokeapi
 from config import MESSAGES
+from game.player import PLAYER
 from game.pokemon import Pokemon
 
 from states import State, StateOptions
@@ -26,8 +28,10 @@ class Battle(State):
     def __init__(self):
         super().__init__()
         self.enemy = asyncio.run(pokeapi.get_random_pokemon_from_api())
+        is_escape = False
         print(MESSAGES["BATTLE"]["ENTRY"].format(self.enemy.name), end="\n" * 2)
-        while self.option != BattleOptions.RUN:
+        print(f"Go {PLAYER.lead_pokemon.name}! \n")
+        while is_escape is not True:
             self.option = self.check_input(BattleOptions)
             # TODO: Implement battling
             if self.option == BattleOptions.FIGHT:
@@ -36,3 +40,18 @@ class Battle(State):
                 pass
             elif self.option == BattleOptions.CATCH:
                 pass
+            elif self.option == BattleOptions.RUN:
+                is_escape = self.calc_run_prob()
+
+    def calc_run_prob(self) -> bool:
+        """Calculates run failure as a percentage of enemy health to player health and caps it at 90%"""
+
+        run_calc = min(self.enemy.health / PLAYER.lead_pokemon.health, 0.9)
+        run_chance = random.random()
+        if run_chance > run_calc:
+            print("Got away safely! \n")
+            return True
+        else:
+            print("Failed to run!")
+            print(f"Wild {self.enemy.name} attacked! \n")
+            return False
